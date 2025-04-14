33e5cfdd359d06a120d915ac2c3740573e98d94e
Owen Halliday <hallidayo@moravian.edu>
2025-04-14 12:08:00 -0400

    modify date to handle dates with year 0

--- utils/date.py
+++ utils/date.py
@@ -1,7 +1,15 @@
 from datetime import datetime
 
 
-def parse_date(date_str):
-    if date_str:
-        return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")
+# Function to parse date fields
+def parse(date: str):
+    if date:
+        if date.startswith(
+            "0000"
+        ):  # date can be 0000-12-30T00:00:00Z in certain cases, like postedDate in the comment CMS-2009-0058-DRAFT-2645
+            date = date.replace(
+                "0000", "2000"
+            )  # this is the behavior from regulations.gov's website,
+            # i believe this may be because the comment was withdrawn...
+        return datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
     return None
