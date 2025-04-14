4678147be35ecfa1c0487985d38a19db1f2d344a
Owen Halliday <hallidayo@moravian.edu>
2025-04-14 13:34:48 -0400

    rename to document_attributes

--- utils/dummy_docket.py
+++ utils/dummy_docket.py
@@ -8,17 +8,17 @@ def get_docket_id(document_id: str):
     return re.sub(pattern, "", document_id)
 
 
-def create(document: dict):
-    docket_id = document.get("docketId", get_docket_id(document.get("documentId")))
+def create(document_attributes: dict):
+    docket_id = document_attributes.get("docketId", get_docket_id(document_attributes.get("documentId")))
     return {
         "data": {
             "id": docket_id,
             "type": "dockets",
             "attributes": {
-                "agencyId": document.get("agencyId"),
-                "docketType": f"Dummy ({document.get('documentType')})",  # as far as i know these can be any of the following: `Rule`, `Other`, and `Notice`
-                "modifyDate": document.get("modifyDate"),
-                "title": document.get("title"),
+                "agencyId": document_attributes.get("agencyId"),
+                "docketType": f"Dummy ({document_attributes.get('documentType')})",  # as far as i know these can be any of the following: `Rule`, `Other`, and `Notice`
+                "modifyDate": document_attributes.get("modifyDate"),
+                "title": document_attributes.get("title"),
             },
             "links": {"self": f"https://api.regulations.gov/v4/dockets/{docket_id}"},
         }
