c732be98da1f36496ff38eb08224837da60b70c7
Owen Halliday <hallidayo@moravian.edu>
2025-04-14 12:50:23 -0400

    fix formatting with comments

--- utils/date.py
+++ utils/date.py
@@ -4,12 +4,9 @@ from datetime import datetime
 # Function to parse date fields
 def parse(date: str):
     if date:
-        if date.startswith(
-            "0000"
-        ):  # date can be 0000-12-30T00:00:00Z in certain cases, like postedDate in the comment CMS-2009-0058-DRAFT-2645
-            date = date.replace(
-                "0000", "2000"
-            )  # this is the behavior from regulations.gov's website,
-            # i believe this may be because the comment was withdrawn...
+        # date can be 0000-12-30T00:00:00Z in certain cases, like postedDate in the comment CMS-2009-0058-DRAFT-2645
+        if date.startswith("0000"):  
+            # this mimics the behavior from regulations.gov's website,
+            date = date.replace("0000", "2000")
         return datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
     return None
