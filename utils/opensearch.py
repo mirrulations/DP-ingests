a5b129ae575de0f5a31ccf23838ccd03ca80bef9
Owen Halliday <hallidayo@moravian.edu>
2025-04-22 18:34:20 -0400

    dont use_ssl in dev

--- utils/opensearch.py
+++ utils/opensearch.py
@@ -29,7 +29,7 @@ def connect():
             hosts=[{'host': host, 'port': port}],
             http_compress=True,  # enables gzip compression for request bodies
             http_auth=auth,
-            use_ssl=True,
+            use_ssl=False,
             verify_certs=False,
             ssl_assert_hostname=False,
             ssl_show_warn=False,
